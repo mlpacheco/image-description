f5ee7e6a7aeca744c0e06f3fd110a71db9ee348c
ML <emele18@gmail.com>
2015-10-29 17:05:27 -0400

    parsing and writing of abstract sentences working

--- src/train_nn.py
+++ src/train_nn.py
@@ -1,31 +1,62 @@
-def parse_microsoft_sentences():
-    pass
+import optparse
+import os
+import json
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
 
-def parse_flickr30k_sentences():
-    pass
+def parse_microsoft_sentences(domain):
+    filename1 = os.path.join(domain, "SimpleSentences", "SimpleSentences1_10020.txt")
+    filename2 = os.path.join(domain, "SimpleSentences", "SimpleSentences2_10020.txt")
+    sentences = {}
+    for filename in [filename1, filename2]:
+        with open(filename) as f:
+            f = f.readlines()
+            for s in f:
+                s = s.strip().split("\t", 2)
+                if s[0] in sentences:
+                    sentences[s[0]] += " " + s[2]
+                else:
+                    sentences[s[0]] = s[2]
 
-def parse_microsoft_images():
-    pass
-
-def parse_flickr30k_images():
-    pass
-
-def parse_sentences():
-    sentences = []
-    parsers = [parse_microsoft_sentences,
-               parse_flickr_sentences]
-    for p in parsers:
-        sentences += p()
     return sentences
 
-def get_sentence_features(sentences):
+
+def parse_flickr30k_sentences(domain):
     pass
 
-def save_sentence_features(filename):
-    pass 
+def parse_microsoft_images(domain):
+    pass
+
+def parse_flickr30k_images(domain):
+    pass
+
+def train_sentence_features(sentences):
+    vectorizer = CountVectorizer()
+    freq = vectorizer.fit_transform(sentences.values())
+    sums = np.asarray(freq.sum(axis=0)).ravel()
+    words = vectorizer.get_feature_names()
+    dictionary = dict(zip(words, sums))
+    return dictionary
+
+def save_sentence_features(dictionary, filename):
+    with open(filename, 'w') as outfile:
+        json.dump(dictionary, outfile)
 
 def main():
-    pass
+    parser = optparse.OptionParser()
+    parser.add_option('-s', '--source', help='path to source domain dir',
+                      dest='source', type='string')
+    parser.add_option('-t', '--target', help='path to target domain dir',
+                      dest='target', type='string')
+    parser.add_option('-o', '--out', help='path to output dir for train instances',
+                      dest='output', type='string')
+    (opts, args) = parser.parse_args()
 
-if __name__ == "__main__"
+    source_sent = parse_microsoft_sentences(opts.source)
+    feat_dict = train_sentence_features(source_sent)
+    save_sentence_features(feat_dict, opts.output)
+
+    return
+
+if __name__ == "__main__":
     main()
