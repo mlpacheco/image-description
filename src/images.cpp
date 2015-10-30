8e049df8f0da39bed9dd7134722b67b00881e286
ML <emele18@gmail.com>
2015-10-30 00:15:09 -0400

    sample code for training sift feats working

--- src/images.cpp
+++ src/images.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <iostream>
 
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
@@ -12,45 +13,57 @@
 using namespace std;
 using namespace cv;
 
-int main(int argc, char *argv[]) {
-
-    string path = "/Users/marialeonor/Purdue/MachineLearning/Repositories/image-description/data/AbstractScenes/RenderedScenes/";
-    string filename = "";
+int trainSift(vector<String> imagePaths, int numWords, string outfile) {
     Mat input;
     vector<KeyPoint> keypoints;
     Mat descriptor;
     Mat featuresUnclustered;
     SiftDescriptorExtractor detector;
 
+    for (int i = 0; i < imagePaths.size(); i++) {
+        cout << imagePaths[i] << endl;
+        input = imread(imagePaths[i], CV_LOAD_IMAGE_GRAYSCALE);
+        detector.detect(input, keypoints);
+        detector.compute(input, keypoints, descriptor);
+        featuresUnclustered.push_back(descriptor);
+
+    }
+    // parameters for the algorithm
+    // 100 iters
+    // epsilon 0.001 ?
+    TermCriteria tc(CV_TERMCRIT_ITER, 100, 0.001);
+    int retries = 1;
+    int flags = KMEANS_PP_CENTERS;
+    // Train K-Means on numWords
+    BOWKMeansTrainer bowTrainer(numWords, tc, retries, flags);
+    cout << featuresUnclustered.dims << endl;
+    Mat dictionary = bowTrainer.cluster(featuresUnclustered);
+    // Write results;
+    FileStorage fs(outfile, FileStorage::WRITE);
+    fs << "vocabulary" << dictionary;
+    fs.release();
+
+    return 1;
+}
+
+
+
+int main(int argc, char *argv[]) {
+
+    string path = "/Users/marialeonor/Purdue/MachineLearning/Repositories/image-description/data/AbstractScenes/RenderedScenes/";
+    string filename;
+    vector<string> imagePaths;
 
     for(int i = 0; i < 10; i++) {
         stringstream ss;
         ss << i;
-        filename = path + "Scene0_" + ss.str();
-        input = imread(filename, CV_LOAD_IMAGE_GRAYSCALE);
-        detector.detect(input, keypoints);
-        detector.compute(input, keypoints, descriptor);
-        featuresUnclustered.push_back(descriptor);
+        filename = path + "Scene0_" + ss.str() + ".png";
+        imagePaths.push_back(filename);
     }
 
-    // I havent looked at the part below yet
+    cout << "imagePaths " << imagePaths.size() << endl;
 
-    //Construct BOWKMeansTrainer
-    //the number of bags
-    int dictionarySize=200;
-    //define Term Criteria
-    TermCriteria tc(CV_TERMCRIT_ITER,100,0.001);
-    //retries number
-    int retries=1;
-    //necessary flags
-    int flags=KMEANS_PP_CENTERS;
-    //Create the BoW (or BoF) trainer
-    BOWKMeansTrainer bowTrainer(dictionarySize,tc,retries,flags);
-    //cluster the feature vectors
-    Mat dictionary=bowTrainer.cluster(featuresUnclustered);
-    //store the vocabulary
-    FileStorage fs("dictionary.yml", FileStorage::WRITE);
-    fs << "vocabulary" << dictionary;
-    fs.release();
+    trainSift(imagePaths, 2, "/Users/marialeonor/Purdue/MachineLearning/Repositories/image-description/out/SIFTwords.yml");
+
 
 }
